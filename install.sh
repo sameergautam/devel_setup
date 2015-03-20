4ac5b2db5c79d496e8eb0e935816dd5e838f1851
Sameer Gautam <sameergautam@outlook.com>
2015-03-20 12:21:47 +0545

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.devel_setup" ]; then
+    echo "Setting up your devel machine"
+    git clone https://github.com/sameergautam/devel_setup.git "$HOME/.devel_setup"
+    cd "$HOME/.devel_setup"
+    LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 rake install
+else
+    echo "Hmmm... Already setup, eh!"
+fi
