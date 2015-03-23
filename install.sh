68fe677dabaffb94992513c652e80685a341d04d
Sameer Gautam <sameergautam@outlook.com>
2015-03-23 14:00:00 +0545

    Quiet clone; as well as clone ernie's setup for the required files and avoid curl for every file required

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 if [ ! -d "$HOME/.devel_setup" ]; then
     echo "Setting up your devel machine"
-    git clone https://github.com/sameergautam/devel_setup.git "$HOME/.devel_setup"
+    git clone -q https://github.com/sameergautam/devel_setup.git "$HOME/.devel_setup"
+    git clone -q https://github.com/ernie/the_setup.git "$HOME/.devel_setup"
     cd "$HOME/.devel_setup"
     LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 rake install
 else
